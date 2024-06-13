34068b45fd1663842f424882cc75c21c6dd67f46
LucasRbnc <lucas.robertonascimento01@gmail.com>
2024-06-13 00:26:54 -0300

    Main

--- src/repositories/ManagerRepository.ts
+++
@@ -1,9 +0,0 @@
-import {Request, Response} from 'express';
-import MongoRespository from "./MongoRepository";
-
-export default class ManagerRepository extends MongoRespository{
-
-    public async createManager(){
-        
-    }
-}
